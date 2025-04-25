fa344c96875f64a12450022cc3bb1d35fcadd87f
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-25 11:59:42 +0800

    Add image

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
+const imgLogo = require("../assets/BU_logo.png");
 
 const Home = () => {
   return (
     <View style={{ flex: 1, backgroundColor: "white", padding: 20 }}>
+      <ImageBackground
+        style={{ width: 200, height: 200 }}
+        source={{ uri: "https://picsum.photos/300" }}
+      ></ImageBackground>
       <Text>
-        {" "}
         <Text style={{ color: "red" }}>Hello</Text> World
       </Text>
+      <View>
+        <Image source={imgLogo} />
+      </View>
     </View>
   );
 };
