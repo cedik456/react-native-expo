4978e66536d1f23562a910400f300bbe18171a16
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-25 12:07:08 +0800

    Add scroll view

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,69 @@
-import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  ScrollView,
+} from "react-native";
 const imgLogo = require("../assets/BU_logo.png");
 
 const Home = () => {
   return (
     <View style={{ flex: 1, backgroundColor: "white", padding: 20 }}>
-      <ImageBackground
+      {/* <ImageBackground
         style={{ width: 200, height: 200 }}
         source={{ uri: "https://picsum.photos/300" }}
-      ></ImageBackground>
-      <Text>
-        <Text style={{ color: "red" }}>Hello</Text> World
-      </Text>
-      <View>
-        <Image source={imgLogo} />
-      </View>
+      ></ImageBackground> */}
+      <ScrollView>
+        <Text>
+          <Text style={{ color: "red" }}>Hello</Text> World
+        </Text>
+        <View>
+          <Image source={imgLogo} />
+        </View>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat et,
+          eveniet iure nemo nesciunt odit omnis fuga maxime voluptatum harum
+          officia quod impedit eius ex laudantium autem mollitia provident
+          rerum. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+          esse laborum, quae voluptas ipsa, eaque accusamus dicta aliquid
+          commodi, aperiam amet tempore vel similique impedit praesentium est
+          fugit iste asperiores! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Fugiat et, eveniet iure nemo nesciunt odit omnis
+          fuga maxime voluptatum harum officia quod impedit eius ex laudantium
+          autem mollitia provident rerum. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Eius esse laborum, quae voluptas ipsa,
+          eaque accusamus dicta aliquid commodi, aperiam amet tempore vel
+          similique impedit praesentium est fugit iste asperiores! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Fugiat et, eveniet iure
+          nemo nesciunt odit omnis fuga maxime voluptatum harum officia quod
+          impedit eius ex laudantium autem mollitia provident rerum. Lorem,
+          ipsum dolor sit amet consectetur adipisicing elit. Eius esse laborum,
+          quae voluptas ipsa, eaque accusamus dicta aliquid commodi, aperiam
+          amet tempore vel similique impedit praesentium est fugit iste
+          asperiores! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Fugiat et, eveniet iure nemo nesciunt odit omnis fuga maxime
+          voluptatum harum officia quod impedit eius ex laudantium autem
+          mollitia provident rerum. Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Eius esse laborum, quae voluptas ipsa, eaque
+          accusamus dicta aliquid commodi, aperiam amet tempore vel similique
+          impedit praesentium est fugit iste asperiores! Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Fugiat et, eveniet iure nemo
+          nesciunt odit omnis fuga maxime voluptatum harum officia quod impedit
+          eius ex laudantium autem mollitia provident rerum. Lorem, ipsum dolor
+          sit amet consectetur adipisicing elit. Eius esse laborum, quae
+          voluptas ipsa, eaque accusamus dicta aliquid commodi, aperiam amet
+          tempore vel similique impedit praesentium est fugit iste asperiores!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat et,
+          eveniet iure nemo nesciunt odit omnis fuga maxime voluptatum harum
+          officia quod impedit eius ex laudantium autem mollitia provident
+          rerum. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+          esse laborum, quae voluptas ipsa, eaque accusamus dicta aliquid
+          commodi, aperiam amet tempore vel similique impedit praesentium est
+          fugit iste asperiores!
+        </Text>
+      </ScrollView>
     </View>
   );
 };
