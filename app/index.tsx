19d7cdeff655e2de61a15caee81cca59090819e3
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-25 11:42:52 +0800

    Add nested text

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Home = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: "blue" }}>
-      <View style={{ backgroundColor: "red", width: 100, height: 100 }}></View>
-      <View
-        style={{ backgroundColor: "green", width: 100, height: 100 }}
-      ></View>
+    <View style={{ flex: 1, backgroundColor: "white", padding: 20 }}>
+      <Text>
+        {" "}
+        <Text style={{ color: "red" }}>Hello</Text> World
+      </Text>
     </View>
   );
 };
