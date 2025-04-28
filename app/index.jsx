03f3cd005423b9bb4fcf777091175f84e8efac0f
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-28 15:21:49 +0800

    Add status bar and logic

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,49 @@
-import { StyleSheet, Text, View } from "react-native";
-const Colon = require("../assets/Colon.png");
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+  Button,
+  Pressable,
+  Text,
+} from "react-native";
 
 const Home = () => {
+  const [statusBarHidden, setIsStatusBarHidden] = useState(false);
+  const [statusBarColor, setIsStatusBarColor] = useState("black");
+
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <StatusBar
+        hidden={statusBarHidden}
+        backgroundColor={statusBarColor}
+        barStyle={statusBarColor === "black" ? "dark-content" : "light-content"}
+      />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 10,
+        }}
+      >
+        <Pressable
+          onPress={() => setIsStatusBarHidden(!statusBarHidden)}
+          style={{ backgroundColor: "black", padding: 10, borderRadius: 30 }}
+        >
+          <Text style={{ color: "white" }}>Click me</Text>
+        </Pressable>
+        <Pressable
+          onPress={() =>
+            setIsStatusBarColor((prevColor) =>
+              prevColor === "black" ? "white" : "black"
+            )
+          }
+          style={{ backgroundColor: "black", padding: 10, borderRadius: 30 }}
+        >
+          <Text style={{ color: "white" }}>Change Color</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
