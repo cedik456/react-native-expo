673bdd3db42f6842a6bfa82047155213e964d8c6
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-05-03 22:31:37 +0800

    Update index.js

--- app/index.jsx
+++ app/index.jsx
@@ -4,13 +4,9 @@ import Box from "../components/Box";
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "yellow", alignSelf: "center" }}>
-        Box 1
-      </Box>
-      <Box style={{ backgroundColor: "red", alignSelf: "flex-end" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "green", alignSelf: "flex-start" }}>
-        Box 3
-      </Box>
+      <Box style={{ backgroundColor: "yellow" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "red" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 3</Box>
       <Box style={{ backgroundColor: "pink", alignSelf: "stretch" }}>Box 4</Box>
       <Box style={{ backgroundColor: "blue", alignSelf: "auto" }}>Box 5</Box>
     </View>
@@ -21,12 +17,12 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    alignItems: "stretch",
+    height: 300,
+    flexWrap: "wrap",
     marginTop: 70,
     backgroundColor: "white",
     borderColor: "grey",
     borderWidth: 5,
-    flex: 1,
+    // flex: 1,
   },
 });
