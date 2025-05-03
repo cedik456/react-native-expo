9c6e064c8701584b8fc9ecf57498db629c9af00e
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-05-03 22:01:26 +0800

    Learn flex direction

--- app/index.jsx
+++ app/index.jsx
@@ -4,11 +4,11 @@ import Box from "../components/Box";
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "yellow", flex: 1 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "yellow" }}>Box 1</Box>
       <Box style={{ backgroundColor: "red" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "green", flex: 4 }}>Box 3</Box>
-      <Box style={{ backgroundColor: "pink", flex: 3 }}>Box 4</Box>
-      <Box style={{ backgroundColor: "blue" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 3</Box>
+      {/* <Box style={{ backgroundColor: "pink", flex: 3 }}>Box 4</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 5</Box> */}
     </View>
   );
 };
@@ -17,6 +17,7 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "column-reverse",
     marginTop: 70,
     backgroundColor: "white",
     borderColor: "grey",
