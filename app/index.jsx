3610e5d55136206d4961ca732e20c13840bb7648
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-05-03 22:06:07 +0800

    Learn justify content

--- app/index.jsx
+++ app/index.jsx
@@ -7,8 +7,8 @@ const Home = () => {
       <Box style={{ backgroundColor: "yellow" }}>Box 1</Box>
       <Box style={{ backgroundColor: "red" }}>Box 2</Box>
       <Box style={{ backgroundColor: "green" }}>Box 3</Box>
-      {/* <Box style={{ backgroundColor: "pink", flex: 3 }}>Box 4</Box>
-      <Box style={{ backgroundColor: "blue" }}>Box 5</Box> */}
+      <Box style={{ backgroundColor: "pink" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 5</Box>
     </View>
   );
 };
@@ -17,7 +17,7 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "column-reverse",
+    justifyContent: "center",
     marginTop: 70,
     backgroundColor: "white",
     borderColor: "grey",
