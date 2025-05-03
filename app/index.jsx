5ab33f49317848ac9d6e7764bb38ab4d6ee94b6a
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-05-03 21:07:48 +0800

    Learn style inheritance

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,21 @@ const Colon = require("../assets/Colon.png");
 const Home = () => {
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="small" color="black" />
+      <View
+        style={{
+          backgroundColor: "black",
+          marginBottom: 10,
+          borderRadius: 6,
+          paddingVertical: 2,
+          paddingHorizontal: 4,
+        }}
+      >
+        <Text style={{ color: "white" }}>
+          Is this <Text style={{ fontWeight: "bold" }}>inherited?</Text>
+        </Text>
+      </View>
+      {/* <ActivityIndicator size="small" color="black" /> */}
+      <View style={[styles.box, styles.boxShadow]}></View>
     </View>
   );
 };
@@ -19,4 +33,23 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: "white",
   },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "grey",
+    borderRadius: 6,
+  },
+  boxShadow: {
+    shadowColor: "#333333",
+    shadowOffset: {
+      width: 6,
+      height: 6,
+    },
+    shadowOpacity: 6,
+    shadowRadius: 4,
+  },
+  // for android specifically
+  // androidShadow: {
+  //   elevation: 10,
+  // },
 });
