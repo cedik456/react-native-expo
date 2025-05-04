02efb416a1fcff3d586cf3eb7725bdbe7234b1ad
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-05-04 11:25:40 +0800

    Update index.js

--- app/index.jsx
+++ app/index.jsx
@@ -4,11 +4,11 @@ import Box from "../components/Box";
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "yellow" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "red" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "green" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "pink", alignSelf: "stretch" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "blue", alignSelf: "auto" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "yellow", flexShrink: 1 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "red", flexShrink: 2 }}>Box 2</Box>
+      {/* <Box style={{ backgroundColor: "green" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "pink" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 5</Box> */}
     </View>
   );
 };
@@ -17,16 +17,15 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
+    width: 300,
     height: 200,
-    flexWrap: "wrap",
+    alignItems: "flex-start",
+    flexDirection: "row",
     marginTop: 70,
-    gap: 5,
-    columnGap: 10,
-    rowGap: 10,
     backgroundColor: "white",
     borderColor: "grey",
     borderWidth: 5,
-    alignContent: "stretch",
-    // flex: 1,
+    // alignContent: "stretch",
+    flex: 1,
   },
 });
