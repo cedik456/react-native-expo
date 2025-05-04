43edbe7da3aa30706cdc72012cf04f2e22d3f0f7
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-05-04 12:10:29 +0800

    Use a manual subscription of dimensions API

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,42 @@
-import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 const Home = () => {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get("window"),
+  });
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener("change", ({ window }) => {
+      setDimensions({ window });
+    });
+    return () => subscription?.remove();
+  });
+
+  const window = dimensions;
+  const windowWidth = window.width;
+  const windowHeight = window.height;
   return (
     <View style={styles.container}>
-      {/* <ActivityIndicator size="small" color="black" /> */}
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 600 ? "60%" : "80%",
+          },
+        ]}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: windowWidth > 500 ? 50 : 24,
+            fontWeight: "bold",
+          }}
+        >
+          Box
+        </Text>
+      </View>
     </View>
   );
 };
@@ -17,5 +50,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 20,
     backgroundColor: "white",
+  },
+  box: {
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
   },
 });
