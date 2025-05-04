304bce8f727c23dc9c492c11e1bcb2eda765b790
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-05-04 11:39:09 +0800

    Update index.js

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,9 @@
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
-const Colon = require("../assets/Colon.png");
+import { StyleSheet, View } from "react-native";
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="small" color="black" />
+      {/* <ActivityIndicator size="small" color="black" /> */}
     </View>
   );
 };
