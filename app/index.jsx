3061bdbde06ade7ecbf021f30b696acae572bb88
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-05-04 13:02:40 +0800

    Learn how to use safe area view

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,44 @@
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  SafeAreaView,
+} from "react-native";
 const Colon = require("../assets/Colon.png");
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <ActivityIndicator size="small" color="black" />
-    </View>
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Text style={styles.text}>Welcome</Text>
+        </View>
+        {/* <ActivityIndicator size="small" color="black" /> */}
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Home;
 
 const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     padding: 20,
     backgroundColor: "white",
+  },
+  box: {
+    padding: 20,
+    borderWidth: 1,
+    borderRadius: 4,
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
